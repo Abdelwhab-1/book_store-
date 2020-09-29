8e176a2853a4842ab1c0aeef46c1820aed6c621c
Abdelwhab-1 <abdelwahab.elasfr@agr.menofia.edu.eg>
2020-09-28 19:08:09 -0700

    fixed the unit test and added the delete function  with its test

--- model/data/data.go
+++ model/data/data.go
@@ -21,9 +21,6 @@ type Dbinterface struct {
 func (dbi Dbinterface) FindUserById(id int) (user *User, err error) {
 	user = new(User)
 	row := dbi.Db.QueryRow("SELECT fname,lname,email,phone FROM users WHERE id=$1", id)
-	if err != nil {
-		return nil, err
-	}
 	err = row.Scan(&user.Fname, &user.Lname, &user.Email, &user.Phone)
 	if err != nil {
 		return nil, err
@@ -43,6 +40,25 @@ func (dbi Dbinterface) CreatUser(user *User) (*sql.Result,*User, error) {
 	}
 	return &result,user , err
 }
-func(Dbinterface) UpdateUser(user *User)(*sql.Result,*User, error){
-	return  nil , nil ,nil
+func(dbi Dbinterface) UpdateUser(user *User)(*sql.Result,*User, error){
+	stmt , err := dbi.Db.Prepare("UPDATE users SET password=$1 ,fname=$2 ,lname=$3 ,email=$4,phone=$5 WHERE  id = $6")
+	if err != nil {
+		return nil , nil ,err
+	}
+	res , err := stmt.Exec(user.Password,user.Fname,user.Lname,user.Email,user.Phone,user.Id)
+	if err != nil {
+		return nil , nil ,err
+	}
+	return &res , user , err
+}
+func(dbi Dbinterface)DeletUser(id  int)(*sql.Result,error){
+	stmt , err := dbi.Db.Prepare("DELETE FROM users  WHERE id = $1")
+	if err != nil {
+		return nil , err
+	}
+	res , err := stmt.Exec(id)
+	if err != nil {
+		return nil , err
+	}
+	return &res ,err
 }
--- model/data/data_test.go
+++ model/data/data_test.go
@@ -41,7 +41,40 @@ func TestFinduserbyid(t *testing.T) {
 		fmt.Println(rows)
 		t.Errorf("error : %v", err)
 	}
-	fmt.Println(user.Id)
+	if err != nil  {
+		if user != nil{
+			t.Errorf("expecting user to be nil found %v",user)
+		}
+	}
+
+}
+
+func TestFinduserbyiderr(t *testing.T) {
+	//initiation
+	u := User{
+		Id:       2139850275894,
+		Password: 1234793857,
+		Fname:    "abdelwhab",
+		Lname:    "elassfer",
+		Email:    "abdo@gmail.com",
+		Phone:    01063260347}
+
+	db, mock := newmock()
+	dbi := Dbinterface{Db: db}
+
+	query := "SELECT  fname,lname,email,phone FROM users WHERE id=\\$1"
+	rows := sqlmock.NewRows([]string{"id","fname", "lname", "email", "phone"}).
+		AddRow(u.Id,u.Fname, u.Lname, u.Email, u.Phone)
+
+	mock.ExpectQuery(query).WillReturnRows(rows)
+	//exeqution
+	user, err := dbi.FindUserById(32445)
+	//validation
+
+	if err == nil  {
+		t.Errorf("should hav  error  we could not find  the user %v", user)
+	}
+
 
 }
 
@@ -70,18 +103,75 @@ func TestCreatuser(t *testing.T) {
 
 }
 
-func TestUpdateuser(t *testing.T){
-  db , mock := newmock()
-  dbi := Dbinterface{Db: db}
-  u := &User{
-    Password: 1234793857,
-    Fname:    "abdelwhab",
-    Lname:    "elassfer",
-    Email:    "abdo@gmail.com",
-    Phone:    01063260347}
-  query := "DELETE FORM users WHERE id =$1"
-  stmt := mock.ExpectPrepare(query)
-  stmt.ExpectExec().WithArgs(u.Id).WillReturnResult(sqlmock.NewResult(0,1 ))
-  res ,us ,err := dbi.UpdateUser(u)
-  fmt.Println(res,us,err)
+func TestUpdateusererr(t *testing.T){
+ db , mock := newmock()
+ dbi := Dbinterface{Db: db}
+ u := &User{
+   Password: 1234793857,
+   Fname:    "abdelwhab",
+   Lname:    "elassfer",
+   Email:    "abdo@gmail.com",
+   Phone:    01063260347}
+ query := "UPDATE users SET password=\\$1 ,fname=\\$2 ,lname=\\$3 ,email=\\$4,phone=\\$5 WHERE  id = \\$6"
+ stmt := mock.ExpectPrepare(query)
+ stmt.ExpectExec().WithArgs(u.Password,u.Fname,u.Lname,u.Email,u.Phone,47512783481295).WillReturnResult(sqlmock.NewResult(0,0 ))
+ _ ,_ ,err := dbi.UpdateUser(u)
+
+ if err == nil {
+ 	t.Errorf("should have an error %v",err)
+ }
 }
+
+
+func TestUpdateuser(t *testing.T){
+	db , mock := newmock()
+	dbi := Dbinterface{Db: db}
+	u := &User{
+		Password: 1234793857,
+		Fname:    "abdelwhab",
+		Lname:    "elassfer",
+		Email:    "abdo@gmail.com",
+		Phone:    01063260347}
+	query := "UPDATE users SET password=\\$1 ,fname=\\$2 ,lname=\\$3 ,email=\\$4,phone=\\$5 WHERE  id = \\$6"
+	stmt := mock.ExpectPrepare(query)
+	stmt.ExpectExec().WithArgs(u.Password,u.Fname,u.Lname,u.Email,u.Phone,u.Id).WillReturnResult(sqlmock.NewResult(0,1 ))
+	res ,u ,err := dbi.UpdateUser(u)
+	if err != nil || u == nil || res == nil {
+		t.Error("coudln't update the user enternal error ")
+	}
+}
+func  TestDeleteuser(t *testing.T){
+	u := &User{
+		Password: 1234793857,
+		Fname:    "abdelwhab",
+		Lname:    "elassfer",
+		Email:    "abdo@gmail.com",
+		Phone:    01063260347}
+	db , mock := newmock()
+	dbi := Dbinterface{db}
+	stmt := mock.ExpectPrepare("DELETE FROM users  WHERE id = \\$1")
+	stmt.ExpectExec().WithArgs(u.Id).WillReturnResult(sqlmock.NewResult(0,1))
+	_ ,err := dbi.DeletUser(u.Id)
+
+	if err  != nil {
+		t.Errorf("should have passed but have error %s",err)
+	}
+}
+func  TestDeleteusererr(t *testing.T){
+	u := &User{
+		Id: 123456789,
+		Password: 1234793857,
+		Fname:    "abdelwhab",
+		Lname:    "elassfer",
+		Email:    "abdo@gmail.com",
+		Phone:    01063260347}
+	db , mock := newmock()
+	dbi := Dbinterface{db}
+	stmt := mock.ExpectPrepare("DELETE FROM users  WHERE id = \\$1")
+	stmt.ExpectExec().WithArgs(59134589732598).WillReturnResult(sqlmock.NewResult(0,1))
+	_ ,err := dbi.DeletUser(u.Id)
+	if err  == nil {
+		t.Errorf("should have  error  %v",err)
+	}
+}
+
